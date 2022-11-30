ba607ef11a8a48e5cd9056efd67acc1559913c21
fabricio <fabricio.gazzola@gmail.com>
2022-11-30 07:45:41 -0300

    Adicionado a estrutura basica de controller controller:users

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,17 @@
+class UsersController {
+/*
+ * index - GET para listar vários registros.
+ * show - GET para exibir um registro específico.
+ * create - POST para criar um registro.
+ * update - PUT para atualizar um registro.
+ * delete - DELETE para remover um registro.
+*/
+
+create (req, res ) {
+    const { name, email , password} = req.body;
+    res.status(201).json({name, email , password});
+}
+
+}
+
+module.exports = UsersController;
